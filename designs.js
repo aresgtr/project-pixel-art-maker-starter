0f336a8b548539a0059fe94c1236325f75e53cca
Qi <784359442@qq.com>
2020-05-27 23:28:27 -0700

    Finish Project

--- designs.js
+++ designs.js
@@ -1,10 +1,44 @@
-// Select color input
+const matrix = document.querySelector('#pixelCanvas');  //  Canvas for our table
+
 // Select size input
+document.getElementById('sizePicker').addEventListener("submit", function(e) {
+    
+    e.preventDefault(); //  Prevent redirect
+    matrix.innerHTML = '';  //  Reset the canvas everytime we click submit
 
-// When size is submitted by the user, call makeGrid()
+    const height = document.getElementById('inputHeight').value;
+    const width = document.getElementById('inputWidth').value;
 
-function makeGrid() {
+    // When size is submitted by the user, call makeGrid()
+    makeGrid(height, width);
+});
 
-// Your code goes here!
 
+/**
+* @description Draw grid matrix on the table canvas, and trigger onClick events to color the selected cell
+* @param {number} height
+* @param {number} width
+*/
+function makeGrid(height, width) {
+
+    var row;
+
+    //  Make Grid table
+    for (var i = 0; i < height; i++) {
+        row = matrix.insertRow(i);
+        for (var j = 0; j < width; j++) {
+            row.insertCell(j);
+        }
+    }
+
+    //  Iterate cells through the table, looking for the onClick event on the cells
+    for (var x = 0; x < matrix.rows.length; x++) {
+        for (var y = 0; y < matrix.rows[x].cells.length; y++) {
+            matrix.rows[x].cells[y].onclick = function() {
+                // Color the cell by delected color input
+                const color = document.getElementById('colorPicker').value;
+                this.style.backgroundColor = color;
+            };
+        }
+    }
 }
